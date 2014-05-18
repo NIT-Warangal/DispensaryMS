72205657c0ee1bc6eb1253e79ed3dbf8482f4de2
kranthikiran01 <guduru.kranthikiran@gmail.com>
2014-05-18 14:37:54 +0530

    Login table in action!!

--- Dispensary.py
+++ Dispensary.py
@@ -13,10 +13,7 @@ app.config.from_object(__name__)
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'Dispensary.db'),
     DEBUG=True,
-    SECRET_KEY='1234567890123456789',
-    USERNAME='root', # Use this in the login screen.
-    PASSWORD='root'  # Use this in the login screen.
-    # We'll port this over to a new Database later.
+    SECRET_KEY=os.urandom(24),
 ))
 app.config.from_envvar('DISPENSARY_SETTINGS', silent=True)
 
@@ -80,15 +77,20 @@ def add_entry():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
+    db=get_db()
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
+    	uname=request.form['username']
+    	pwd=request.form['password']
+	connection = sqlite3.connect(app.config['DATABASE'])
+	cur = connection.cursor()
+	cur.execute('select rowid from Login where UserName=? and Password=?',[uname,pwd])
+	data = cur.fetchone()
+	if data is None:
+	    error='Invalid username/password'
+	else:
+	    session['logged_in'] = True
+	    flash('You were logged in')
+	    return redirect(url_for('show_entries'))
     return render_template('login.html', error=error) #login.html
 
 t=0
@@ -170,4 +172,5 @@ def logout():
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key=app.config['SECRET_KEY']
     app.run()
