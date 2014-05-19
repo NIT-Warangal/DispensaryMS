74c4656a4f526bcc19090afd1a5aa40e24f41a94
sudheesh001 <sudheesh1995@outlook.com>
2014-05-19 18:29:11 +0300

    Refactoring code from SQLite to MySQL [Broken]

--- Dispensary.py
+++ Dispensary.py
@@ -1,62 +1,43 @@
 # all the imports
 import os
-import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-
-
+from flaskext.mysql import MySQL
+import datetime
+import subprocess
+ 
+mysql = MySQL()
 # create our little application :)
 app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'Dispensary'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 app.config.from_object(__name__)
+app.config['USERNAME']=''
 
-# Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'Dispensary.db'),
-    DEBUG=True,
-    USERNAME='',
-    SECRET_KEY=os.urandom(24),
-))
-app.config.from_envvar('DISPENSARY_SETTINGS', silent=True)
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+def get_cursor():
+    return mysql.connect().cursor()
 
 @app.teardown_appcontext
-def close_db(error):
+def close_db(cursor):
     """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+    mysql.connect().close()
 
 @app.route('/')
 def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
+    db = get_cursor()
+    db.execute('select title, text from entries order by id desc')
+    entries = db.fetchall()
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST']) #add
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    db = get_db()
+    db = get_cursor()
     db.execute('insert into Users (Sno, RegNo, \'FirstName\', \'MiddleName\',\'LastName\', \'BloodGroup\', \'DateofBirth\', Age, Type, \'Phonenumber\', Address, email) values (?,?,?,?,?,?,?,?,?,?,?,?)',
         [request.form['Sno'], request.form['RegNo'],request.form['FirstName'],request.form['MiddleName'],request.form['LastName'],request.form['BloodGroup'],request.form['DateOfBirth'],request.form['Age'],request.form['Type'],request.form['PhoneNumber'],request.form['text'],request.form['email']])
     db.commit()
@@ -67,7 +48,7 @@ def add_entry():
 # def add_user():
 #     if not session.get('logged_in'):
 #         abort(401)
-#     db = get_db()
+#     db = get_cursor()
 #     db.execute('insert into Users (Sno, RegNo, First Name, Middle Name,Last Name, Blood Group, Date of Birth, Age, Type, Phone number, Address, email) values (?, ?,?,?,?,?,?,?,?,?,?,?)',
 #         [request.form['Sno'], request.form['RegNo'],request.form['FirstName'],request.form['MiddleName'],request.form['LastName'],request.form['BloodGroup'],request.form['DateOfBirth'],request.form['Age'],request.form['Type'],request.form['PhoneNumber'],request.form['text'],request.form['email']])
 
@@ -78,83 +59,83 @@ def add_entry():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    db=get_db()
+    db=get_cursor()
     if request.method == 'POST':
-        uname=request.form['username']
-        pwd=request.form['password']
-        connection = sqlite3.connect(app.config['DATABASE'])
-        cur = connection.cursor()
-        cur.execute('select rowid from Login where UserName=? and Password=?',[uname,pwd])
-        data = cur.fetchone()
-        if data is None:
-	       error='Invalid username/password'
+        uname=str(request.form['username'])
+        pwd=str(request.form['password'])
+        sql='select Count(*) from Login where UserName="%s" and Password="%s"'%(uname,pwd)
+        db.execute(sql)
+        data = db.fetchone()[0]
+        if data ==0:
+           error='Invalid username/password'
         else:
             session['logged_in'] = True
             app.config['USERNAME'] = uname
-            flash('You were logged in')
-	    return redirect(url_for('show_entries'))
+            flash('You were logged in'+app.config['USERNAME']+str(session['logged_in']))
+        return redirect(url_for('show_entries'))
     return render_template('login.html', error=error) #login.html
 
 t=0
 @app.route('/inventory')
 def inventory():
-    db = get_db()
-    cur = db.execute('select * from Pharmacy order by Sno')
-    entries = cur.fetchall()
+    db = get_cursor()
+    db.execute('select * from Pharmacy order by Sno')
+    entries = db.fetchall()
     return render_template('pharmventory.html',entries = entries)
 
 @app.route('/insert',methods=['GET','POST'])
 def insert():
-	global t
-	db=get_db()
-	cur=db.execute('select count() from pharmacy')
-	t=cur.fetchone()[0]
-	if request.form['btn'] == 'insert':
-		sno = request.form['Sno']
-		name = request.form['Name']
-		quantity = request.form['qty']
-		batchno = request.form['bno']
-		mfg = request.form['mfgdate']
-		exp = request.form['expdate']
-		db.execute('insert into pharmacy values(?,?,?,?,?,?)',[sno,name,quantity,batchno,mfg,exp])
-		db.commit()
-		flash('New entry successfully inserted')
-		return redirect(url_for('inventory'))
-	else:
-		for i in range( 1, t+1):
-			r = str(i)
-			if request.form['btn'] == 'update' + r:
-				sno=request.form['Sno' + r]
-				name=request.form['Name' + r]
-				quantity=request.form['qty' + r]
-				batchno=request.form['bno' + r]
-				mfg = request.form['mfgdate' + r]
-				exp = request.form['expdate' + r]
-				query = 'update pharmacy set Name=?,Quantity=?,Batchno=?,ManufactureDate=?,ExpiryDate=? where Sno=?'
-				db.execute(query,[name,quantity,batchno,mfg,exp,sno])
-				db.commit()
-				flash('Record '+sno+' updated')
-				return redirect(url_for('inventory'))		
-			elif request.form['btn'] == 'delete' + r:
-				sno=request.form['Sno' + r]
-				query='delete from pharmacy where Sno=?'
-				db.execute(query,[sno])
-				db.commit()
-				flash('Record '+sno+' deleted')
-				return redirect(url_for('inventory'))
-	flash('Nothing occured'+request.form['btn'])
-	return redirect(url_for('inventory'))
+    global t
+    db=get_cursor()
+    db.execute('select Count(1) from Pharmacy')
+    t=db.fetchone()[0]
+    flash(t)
+    #t=db.fetchall[0][0]
+    if request.form['btn'] == 'insert':
+        sno = int(request.form['Sno'])
+        name = str(request.form['Name'])
+        quantity = int(request.form['qty'])
+        batchno = request.form['bno']
+        mfg = datetime.datetime.strptime(request.form['mfgdate'])
+        exp = datetime.datetime.strptime(request.form['expdate'])
+        db.execute('insert into Pharmacy values(%s,%s,%s,%s,%s,%s)'%(sno,name,quantity,batchno,mfg.isoformat(),exp.isoformat()))
+        flash('New entry successfully inserted')
+        return redirect(url_for('inventory'))
+    else:
+        for i in range( 1, t+1):
+            r = str(i)
+            if request.form['btn'] == 'update' + r:
+                sno=request.form['Sno' + r]
+                name=request.form['Name' + r]
+                quantity=request.form['qty' + r]
+                batchno=request.form['bno' + r]
+                mfg = request.form['mfgdate' + r]
+                exp = request.form['expdate' + r]
+                query = 'update pharmacy set Name=?,Quantity=?,Batchno=?,ManufactureDate=?,ExpiryDate=? where Sno=?'
+                db.execute(query,[name,quantity,batchno,mfg,exp,sno])
+                db.commit()
+                flash('Record '+sno+' updated')
+                return redirect(url_for('inventory'))       
+            elif request.form['btn'] == 'delete' + r:
+                sno=request.form['Sno' + r]
+                query='delete from pharmacy where Sno=?'
+                db.execute(query,[sno])
+                db.commit()
+                flash('Record '+sno+' deleted')
+                return redirect(url_for('inventory'))
+    flash('Nothing occured'+request.form['btn'])
+    return redirect(url_for('inventory'))
 
 @app.route('/prescription')
 def prescription():
-    db = get_db()
+    db = get_cursor()
     cur = db.execute('select * from Prescription order by RegNo asc')
     entries = cur.fetchall()
     return render_template('prescription.html',entries = entries)
 
 @app.route('/fileprescription', methods=['GET','POST'])
 def fileprescription():
-    db = get_db()
+    db = get_cursor()
     docno = request.form['DoctorNo']
     regno = request.form['RegNo']
     cause = request.form['Cause']
@@ -168,7 +149,7 @@ def fileprescription():
 
 @app.route('/employee',methods=['GET','POST'])
 def employee():
-    db = get_db()
+    db = get_cursor()
     error=None
     chars=[chr(i) for i in xrange(ord('A'), ord('N')+1)]
     query = 'select EmpID from Login where UserName=?'
@@ -188,7 +169,7 @@ def employeeinfo():
 
 @app.route('/student',methods=['GET','POST'])
 def student():
-    db = get_db()
+    db = get_cursor()
     error=None
     chars=[chr(i) for i in xrange(ord('A'), ord('N')+1)]
     query = 'select EmpID from Login where UserName=?'
@@ -214,5 +195,5 @@ def logout():
 
 if __name__ == '__main__':
     app.debug = True
-    app.secret_key=app.config['SECRET_KEY']
+    app.secret_key=os.urandom(24)
     app.run()
---
+++ Dispensary.sql
@@ -0,0 +1,143 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2014 at 02:26 PM
+-- Server version: 5.5.37-0ubuntu0.14.04.1
+-- PHP Version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `Dispensary`
+--
+
+-- --------------------------------------------------------
+create table `entries` (
+  `id` integer primary key autoincrement,
+  `title` text not null,
+  `text` text not null
+);
+--
+-- Table structure for table `Employee`
+--
+
+CREATE TABLE IF NOT EXISTS `Employee` (
+  `RegNo` text NOT NULL,
+  `Occupation` text NOT NULL,
+  `Location` text NOT NULL,
+  `NoOfDependencies` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Employee`
+--
+
+INSERT INTO `Employee` (`RegNo`, `Occupation`, `Location`, `NoOfDependencies`) VALUES
+('127223', 'emp', 'secunderabad', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `HostelWorker`
+--
+
+CREATE TABLE IF NOT EXISTS `HostelWorker` (
+  `RegNo` text NOT NULL,
+  `Occupation` text NOT NULL,
+  `Location` text NOT NULL,
+  `NoOfDependencies` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Login`
+--
+
+CREATE TABLE IF NOT EXISTS `Login` (
+  `EmpID` text NOT NULL,
+  `UserName` text NOT NULL,
+  `Password` text NOT NULL,
+  `Occupation` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Pharmacy`
+--
+
+CREATE TABLE IF NOT EXISTS `Pharmacy` (
+  `Sno` int(11) NOT NULL,
+  `Name` text NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Batchno` int(11) NOT NULL,
+  `ManufactureDate` date NOT NULL,
+  `ExpiryDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Prescription`
+--
+
+CREATE TABLE IF NOT EXISTS `Prescription` (
+  `DoctorNo` int(11) NOT NULL,
+  `RegNo` text NOT NULL,
+  `Cause` text NOT NULL,
+  `Medicine` text NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Remarks` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Student`
+--
+
+CREATE TABLE IF NOT EXISTS `Student` (
+  `Year` int(11) NOT NULL,
+  `Section` text NOT NULL,
+  `Branch` text NOT NULL,
+  `Degree` text NOT NULL,
+  `RegNo` text NOT NULL,
+  `RollNo` text NOT NULL,
+  `Hostel` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `Sno` int(11) NOT NULL,
+  `RegNo` text NOT NULL,
+  `FirstName` text NOT NULL,
+  `MiddleName` text,
+  `LastName` text NOT NULL,
+  `BloodGroup` text NOT NULL,
+  `DateofBirth` date NOT NULL,
+  `Age` int(11) NOT NULL,
+  `Type` int(11) NOT NULL,
+  `Phonenumber` text NOT NULL,
+  `Address` text NOT NULL,
+  `email` text NOT NULL,
+  `gender` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
