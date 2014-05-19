022258c53d5f0a3a600321e953a40ea9a5160b1f
sudheesh001 <sudheesh1995@outlook.com>
2014-05-19 21:30:40 +0300

    Clean up

--- Dispensary.py
+++ Dispensary.py
@@ -4,29 +4,17 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 from flaskext.mysql import MySQL
 import datetime
-<<<<<<< HEAD
-import subprocess
-=======
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
  
 mysql = MySQL()
 # create our little application :)
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
-<<<<<<< HEAD
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-=======
-app.config['MYSQL_DATABASE_PASSWORD'] = 'kiran123'
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
 app.config['MYSQL_DATABASE_DB'] = 'Dispensary'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 app.config.from_object(__name__)
 app.config['USERNAME']=''
-<<<<<<< HEAD
-
-=======
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
 
 def get_cursor():
     return mysql.connect().cursor()
@@ -35,10 +23,7 @@ def get_cursor():
 def close_db(cursor):
     """Closes the database again at the end of the request."""
     mysql.connect().close()
-<<<<<<< HEAD
 
-=======
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
 @app.route('/')
 def show_entries():
     db = get_cursor()
@@ -51,11 +36,6 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_cursor()
-<<<<<<< HEAD
-    db.execute('insert into Users (Sno, RegNo, \'FirstName\', \'MiddleName\',\'LastName\', \'BloodGroup\', \'DateofBirth\', Age, Type, \'Phonenumber\', Address, email) values (?,?,?,?,?,?,?,?,?,?,?,?)',
-        [request.form['Sno'], request.form['RegNo'],request.form['FirstName'],request.form['MiddleName'],request.form['LastName'],request.form['BloodGroup'],request.form['DateOfBirth'],request.form['Age'],request.form['Type'],request.form['PhoneNumber'],request.form['text'],request.form['email']])
-    db.commit()
-=======
     sno=request.form['Sno']
     regno= request.form['RegNo']
     fname=request.form['FirstName']
@@ -74,7 +54,6 @@ def add_entry():
      email,gender) values (%s,%s,"%s","%s","%s","%s","%s",%s,%s,"%s","%s","%s","%s")'
     db.execute(sql%(sno,regno,fname,mname,lname,bgroup,dob,age,typ,phn,address,email,gender))
     db.execute("COMMIT")
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))#show_entries
 
@@ -83,11 +62,7 @@ def add_entry():
 #     if not session.get('logged_in'):
 #         abort(401)
 #     db = get_cursor()
-<<<<<<< HEAD
-#     db.execute('insert into Users (Sno, RegNo, First Name, Middle Name,Last Name, Blood Group, Date of Birth, Age, Type, Phone number, Address, email) values (?, ?,?,?,?,?,?,?,?,?,?,?)',
-=======
 #     db.execute('insert into Users (Sno, RegNo, First Name, Middle Name,Last Name, Blood Group, Date of Birth, Age, Type, Phone number, Address, email) values (%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
 #         [request.form['Sno'], request.form['RegNo'],request.form['FirstName'],request.form['MiddleName'],request.form['LastName'],request.form['BloodGroup'],request.form['DateOfBirth'],request.form['Age'],request.form['Type'],request.form['PhoneNumber'],request.form['text'],request.form['email']])
 
 #     db.commit()
@@ -105,31 +80,21 @@ def login():
         db.execute(sql)
         data = db.fetchone()[0]
         if data ==0:
-<<<<<<< HEAD
-           error='Invalid username/password'
-=======
             error='Invalid username/password'
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
         else:
             session['logged_in'] = True
             app.config['USERNAME'] = uname
-            flash('You were logged in'+app.config['USERNAME']+str(session['logged_in']))
+            flash('You were logged in '+app.config['USERNAME']+' '+str(session['logged_in']))
         return redirect(url_for('show_entries'))
     return render_template('login.html', error=error) #login.html
 
 t=0
 @app.route('/inventory')
 def inventory():
-<<<<<<< HEAD
-    db = get_cursor()
-    db.execute('select * from Pharmacy order by Sno')
-    entries = db.fetchall()
-=======
     db=get_cursor()
     db.execute('select * from Pharmacy order by Sno')
     entries = db.fetchall()
     db.execute('commit')
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
     return render_template('pharmventory.html',entries = entries)
 
 @app.route('/insert',methods=['GET','POST'])
@@ -141,15 +106,6 @@ def insert():
     flash(t)
     #t=db.fetchall[0][0]
     if request.form['btn'] == 'insert':
-<<<<<<< HEAD
-        sno = int(request.form['Sno'])
-        name = str(request.form['Name'])
-        quantity = int(request.form['qty'])
-        batchno = request.form['bno']
-        mfg = datetime.datetime.strptime(request.form['mfgdate'])
-        exp = datetime.datetime.strptime(request.form['expdate'])
-        db.execute('insert into Pharmacy values(%s,%s,%s,%s,%s,%s)'%(sno,name,quantity,batchno,mfg.isoformat(),exp.isoformat()))
-=======
         sno = (request.form['Sno'])
         name = (request.form['Name'])
         quantity = (request.form['qty'])
@@ -168,7 +124,6 @@ def insert():
         sql = 'insert into Pharmacy values(%s,"%s",%s,%s,"%s","%s")'
         db.execute(sql%(sno,name,quantity,batchno,mfg,exp))
         db.execute("COMMIT")
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
         flash('New entry successfully inserted')
         return redirect(url_for('inventory'))
     else:
@@ -180,19 +135,6 @@ def insert():
                 quantity=request.form['qty' + r]
                 batchno=request.form['bno' + r]
                 mfg = request.form['mfgdate' + r]
-<<<<<<< HEAD
-                exp = request.form['expdate' + r]
-                query = 'update pharmacy set Name=?,Quantity=?,Batchno=?,ManufactureDate=?,ExpiryDate=? where Sno=?'
-                db.execute(query,[name,quantity,batchno,mfg,exp,sno])
-                db.commit()
-                flash('Record '+sno+' updated')
-                return redirect(url_for('inventory'))       
-            elif request.form['btn'] == 'delete' + r:
-                sno=request.form['Sno' + r]
-                query='delete from pharmacy where Sno=?'
-                db.execute(query,[sno])
-                db.commit()
-=======
                 year=int(mfg[0:4])
                 month=int(mfg[5:7])
                 date=int(mfg[8:10])
@@ -206,13 +148,12 @@ def insert():
                 db.execute(query%(name,quantity,batchno,mfg,exp,sno))
                 db.execute("COMMIT")
                 flash('Record '+sno+' updated')
-                return redirect(url_for('inventory'))		
+                return redirect(url_for('inventory'))       
             elif request.form['btn'] == 'delete' + r:
                 sno=request.form['Sno' + r]
                 query='delete from pharmacy where Sno='+str(sno)
                 db.execute(query)
                 db.execute("COMMIT")
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
                 flash('Record '+sno+' deleted')
                 return redirect(url_for('inventory'))
     flash('Nothing occured'+request.form['btn'])
@@ -221,13 +162,8 @@ def insert():
 @app.route('/prescription')
 def prescription():
     db = get_cursor()
-<<<<<<< HEAD
-    cur = db.execute('select * from Prescription order by RegNo asc')
-    entries = cur.fetchall()
-=======
     db.execute('select * from Prescription order by RegNo asc')
     entries = db.fetchall()
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
     return render_template('prescription.html',entries = entries)
 
 @app.route('/fileprescription', methods=['GET','POST'])
@@ -294,8 +230,4 @@ def logout():
 if __name__ == '__main__':
     app.debug = True
     app.secret_key=os.urandom(24)
-<<<<<<< HEAD
     app.run()
-=======
-    app.run()
->>>>>>> 8a81dc1a9a630eb94b07915f11a077ec56151f19
