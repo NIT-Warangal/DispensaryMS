9806a42745e56463ac73e3e539fec4b589a1dcce
kranthikiran01 <guduru.kranthikiran@gmail.com>
2014-05-23 19:35:29 +0530

    Datepicker UI Fix - Resolved :)

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 config = {
 	'MYSQL_DATABASE_USER' : 'root',
 	'MYSQL_DATABASE_DB' : 'Dispensary',
-	'MYSQL_DATABASE_PASSWORD' : 'root',
+	'MYSQL_DATABASE_PASSWORD' : 'kiran123',
 	'MYSQL_DATABASE_HOST' : 'localhost', 
 	'USERNAME' : '',
 }
