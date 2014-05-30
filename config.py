df03d42b9d8a2fe8ee6f211b5f8e65027733120d
sudheesh001 <sudheesh1995@outlook.com>
2014-05-30 14:45:00 +0300

    Config.py is required, the pyc is not

---
+++ config.py
@@ -0,0 +1,8 @@
+config = {
+	'MYSQL_DATABASE_USER' : 'root',
+	'MYSQL_DATABASE_DB' : 'Dispensary1',
+	'MYSQL_DATABASE_PASSWORD' : 'root',
+	'MYSQL_DATABASE_HOST' : 'localhost', 
+	'USERNAME' : '',
+	'USERID' :'',
+}
