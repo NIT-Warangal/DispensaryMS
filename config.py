beaa6cbd2ce086653688797a9903a35c3a0d68df
sudheesh001 <sudheesh1995@outlook.com>
2014-06-04 00:02:53 +0300

    Final config.py for v1.0 ; skip worktree

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Database Config
 
 config = {
-	'MYSQL_DATABASE_USER' : 'root',
+	'MYSQL_DATABASE_USER' : '', #Username for mysql
 	'MYSQL_DATABASE_DB' : 'Dispensary',
-	'MYSQL_DATABASE_PASSWORD' : 'kiran123',
+	'MYSQL_DATABASE_PASSWORD' : '', # Password to connect to mysql
 	'MYSQL_DATABASE_HOST' : 'localhost', 
 	'USERNAME' : '',
 	'USERID' :'',
